b28856f272a8b0f88e59aee88fb7ffefa3d197c7
柒梦染星河 <xi.xi.li@outlook.com>
2026-04-25 22:29:43 +0800

    移动端禁用波浪动画

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-﻿import type {
+﻿import { tuple } from "astro:schema";
+import type {
 	AnnouncementConfig,
 	CommentConfig,
 	ExpressiveCodeConfig,
@@ -148,7 +149,7 @@ export const siteConfig: SiteConfig = {
 		waves: {
 			enable: true, // 是否启用水波纹效果（注意：此功能性能开销较大）
 			performanceMode: true, // 性能模式：减少动画复杂度(性能提升40%)
-			mobileDisable: false, // 移动端禁用
+			mobileDisable: true, // 移动端禁用
 		},
 
 		// PicFlow API支持(智能图片API)
